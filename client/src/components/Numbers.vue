ccb4368d7e3035a0b4f157d6d291b114349e6492
leozcarvalho <leo.zcarvalho@hotmail.com>
2021-07-16 15:59:52 -0300

    Front evoluido

--- client/src/components/Numbers.vue
+++ client/src/components/Numbers.vue
@@ -15,11 +15,13 @@
                     <div class="col-8">
                         <input class = "form-control form-control-lg"
                         placeholder = "Insira um número inteiro positivo"
+                        v-model="number"
                         maxlength="3">
                     </div>
                     <div class="col-2">
                         <button
-                        class="btn btn-success btn-lg mb-2">
+                        class="btn btn-success btn-lg mb-2"
+                        @click="addNumber">
                         ENVIAR
                         </button>
                     </div>
@@ -29,3 +31,42 @@
         <div class="col-2"></div>
     </div>
 </template>
+
+<script>
+
+/* import axios from 'axios'; */
+
+export default {
+  data() {
+    return {
+      numbers: [],
+      number: null,
+    };
+  },
+
+  methods: {
+    addNumber() {
+      const num = parseInt(this.number, 8);
+      console.log(num);
+      if (Number.isNaN(num)) {
+        window.alert('Insira um valor!');
+      } else if (num < -1) {
+        console.log(typeof this.number);
+        window.alert('Insira um valor positivo!');
+      } else if (num === -1) {
+        this.postNumbers();
+      } else {
+        this.numbers.push(this.number);
+        this.number = null;
+      }
+      console.log(typeof num);
+      console.log(this.numbers);
+    },
+    postNumbers() {
+      console.log('teste');
+    },
+  },
+  created() {
+  },
+};
+</script>
